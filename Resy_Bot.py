c8c433fda6b47b0a2b62220877ce73002dd55645
githubfarid1 <githubfarid1@gmail.com>
2024-06-03 21:14:19 +0900

    update

--- Resy_Bot.py
+++ Resy_Bot.py
@@ -112,9 +112,10 @@ def main(restaurant_link, date_wanted, seats, time_wanted, period_wanted, reserv
 
         # Rotate user agents or use more realistic ones
         user_agents = [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
-            "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+            # "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+            # "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
             # More user agents can be added here
         ]
         user_agent = random.choice(user_agents)
@@ -160,7 +161,7 @@ def main(restaurant_link, date_wanted, seats, time_wanted, period_wanted, reserv
             ] 
             # browser = p.chromium.launch(headless=True, args=browser_args)
 
-            browser = p.webkit.launch()# (headless=headless, args=['--enable-logging=stderr','--v=1'])
+            browser = p.chromium.launch(headless=headless, args=['--enable-logging=stderr','--v=1'])
             proxy_server = "http://kpeqkzlp:0sdrl0jganhc@38.154.227.167:5868"
 
             context = browser.new_context(
